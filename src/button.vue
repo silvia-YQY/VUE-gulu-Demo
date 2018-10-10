1165dc7d71454ccf596ce2f3d0d15ee547dc0c75
silvia-YQY <545067444@qq.com>
2018-10-10 23:25:45 +0800

    设置icon自定义放置左右

--- src/button.vue
+++ src/button.vue
@@ -1,31 +1,59 @@
-
 <template>
-	<button class="g-button">
-        <slot></slot>
-	</button>
+    <button class="g-button" :class="{[iconPosition]:true} ">
+        <svg v-if="icon" class="icon">
+            <use :xlink:href="`#i-${icon}`"></use>
+        </svg>
+        <div class="content">
+            <slot></slot>
+        </div>
+
+    </button>
 </template>
 
 <script>
-export default {}
+    export default {
+        data() {
+            return {}
+        },
+        props: ['icon', 'iconPosition']
+    }
 </script>
 
 <style lang="scss">
-	.g-button{
-			font-size:var(--font-size);
-			height: var(--button-height);
-			background: var(--button-background);
-			padding:0 1em;
-			border: 1px solid var(--border-color);
-			border-radius: var(--border-radius);
-		&:hover{
-				border-color:var(--border-color-hover);
-		}
-		&:active{
-				background-color:var(--botton-active-background);
-		}
-		&:focus{
-				outline:none;
-		}
-	}
+    .g-button {
+        font-size: var(--font-size);
+        height: var(--button-height);
+        background: var(--button-background);
+        padding: 0 1em;
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        &:hover {
+            border-color: var(--border-color-hover);
+        }
+        &:active {
+            background-color: var(--botton-active-background);
+        }
+        &:focus {
+            outline: none;
+        }
+        > .icon {
+            order: 1
+        }
+        > .content {
+            order: 2;
+        }
+        &.icon-right {
+            > .icon {
+                order: 2
+            }
+            > .content {
+                order: 1
+            }
+        }
+
+    }
 </style>
 
