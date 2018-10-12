5ec8ee5456c48f89b179084dda0bf9303361d863
silvia-YQY <545067444@qq.com>
2018-10-12 21:19:13 +0800

    添加按钮组，重点！！&:not(:first-child){                           border-left:none;                       }                       scss选择器not！！！

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from "vue"
 import Button from "./button.vue"
 import Icon from "./icon.vue"
+import ButtonGroup from "./button_group.vue"
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
+Vue.component('g-button',Button);
+Vue.component('g-icon',Icon);
+Vue.component('g-button-group',ButtonGroup);
 
 new Vue({
 	el:"#app",
@@ -12,4 +14,4 @@ new Vue({
 	        loading:false
         }
     }
-})
+});
