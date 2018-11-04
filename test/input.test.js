7624a9c1cc125745a6f02edfbbc72eea2abcef9f
silvia-YQY <545067444@qq.com>
2018-11-04 15:33:59 +0800

    优化测试用例

--- test/input.test.js
+++ test/input.test.js
@@ -80,45 +80,19 @@ describe('Input', () => {
         })
 
 
-        it('支持change事件',()=>{
-            vm = new Constructor({}).$mount()
-            const callback = sinon.fake();
-            vm.$on('change',callback)
-            let event = new Event('change');
-            let inputElement = vm.$el.querySelector('input')
-            inputElement.dispatchEvent(event)
-            expect(callback).to.have.been.calledWith(event)
-        })
+        it('支持change/input/blur事件',()=>{
+            ['change','input','focus','blur']
+                .forEach((eventName)=>{
+                vm = new Constructor({}).$mount()
+                const callback = sinon.fake();
+                vm.$on(eventName,callback)
+                let event = new Event(eventName);
+                let inputElement = vm.$el.querySelector('input')
+                inputElement.dispatchEvent(event)
+                console.log(eventName);
+                expect(callback).to.have.been.calledWith(event)
+            })
 
-
-        it('支持input事件',()=>{
-            vm = new Constructor({}).$mount()
-            const callback = sinon.fake();
-            vm.$on('input',callback)
-            let event = new Event('input');
-            let inputElement = vm.$el.querySelector('input')
-            inputElement.dispatchEvent(event)
-            expect(callback).to.have.been.calledWith(event)
-        })
-
-        it('支持focus事件',()=>{
-            vm = new Constructor({}).$mount()
-            const callback = sinon.fake();
-            vm.$on('focus',callback)
-            let event = new Event('focus');
-            let inputElement = vm.$el.querySelector('input')
-            inputElement.dispatchEvent(event)
-            expect(callback).to.have.been.calledWith(event)
-        })
-
-        it('支持blur事件',()=>{
-            vm = new Constructor({}).$mount()
-            const callback = sinon.fake();
-            vm.$on('blur',callback)
-            let event = new Event('blur');
-            let inputElement = vm.$el.querySelector('input')
-            inputElement.dispatchEvent(event)
-            expect(callback).to.have.been.calledWith(event)
         })
     })
 })
