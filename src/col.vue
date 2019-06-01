085a7e39ceb926601f60786c7ff31bdf9215f0cb
silvia-YQY <545067444@qq.com>
2019-06-01 17:03:42 +0800

    row，col 初版

--- src/col.vue
+++ src/col.vue
@@ -1,16 +1,39 @@
 <template>
-  <div class="col" :class="[`col-${span}`]" >
-    <slot></slot>
+  <div class="col" 
+    :style="{paddingLeft:gutter/2+'px',paddingRight:gutter/2+'px'}"
+    :class="[span && `col-${span}`,offset && `offset-${offset}`]"
+    >
+    <div>
+      <slot></slot>
+    </div>
+    
   </div>
 </template>
 
 <script>
 export default {
-  name:'GuluCol',
-  props:{
-    span:[Number,String]
+  name: "GuluCol",
+  props: {
+    span: {
+      type: [Number, String]
+    },
+    offset: {
+      type: [Number, String]
+    }
+  },
+  data(){
+    return{
+      gutter: 0
+    }
+  },
+  created(){    
+    console.log('col created');
+    // created的时候是没有儿子    
+  },
+  mounted(){
+    console.log('col mounted',this.gutter);
   }
-}
+};
 </script>
 
 
@@ -18,15 +41,23 @@ export default {
 .col {
   height: 100px;
   width: 50%;
-  background: grey;
-  border: 1px solid red;
-
+   
+  div{
+    border: 1px solid red; 
+    background: grey;    
+  }
 
   $class-prefix: col-; // 前缀
-
   @for $n from 1 through 24 {
     &.#{$class-prefix}#{$n} {
       width: ($n/24) * 100%;
+    }
+  }
+
+  $class-prefix: offset-; // 前缀
+  @for $n from 1 through 24 {
+    &.#{$class-prefix}#{$n} {
+      margin-left: ($n/24) * 100%;
     }
   }
 }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,30 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{marginLeft:-gutter/2+'px',marginRight:-gutter/2+'px'}">
     <slot></slot>
   </div>
 </template>
+
+<script>
+export default {
+  name: "GuluRow",
+  props: {
+    gutter: {
+      type: [Number, String]
+    }
+  },
+  created(){    
+    console.log('created');
+    // created的时候是没有儿子    
+  },
+  mounted(){
+    console.log('mounted',this.gutter);
+    // 在row挂载后，给所有子元素传gutter值
+    this.$children.forEach(vm=>{
+      vm.gutter = this.gutter
+    })
+  }
+};
+</script>
 
 <style lang="scss" scoped>
 .row{
