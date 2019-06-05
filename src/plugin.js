aee738bc2be94f3d5434273436ef082f221b4e06
silvia-YQY <545067444@qq.com>
2019-06-05 17:46:57 +0800

    添加toast

---
+++ src/plugin.js
@@ -0,0 +1,15 @@
+
+import Toast from './toast'
+export default {
+  install(Vue, options) {
+    Vue.prototype.$toast = function (msg) {
+      let Constructor = Vue.extend(Toast)  // 继承
+      let toast = new Constructor()  // 创建
+      toast.$slots.default = [msg]  // 给插槽传入默认信息：msg
+      toast.$mount()     // 在vue中挂载（虚
+      let element = document.querySelector('.toast');
+      element.appendChild(toast.$el)  //  在页面挂载（实
+    }
+
+  }
+}
---
+++ src/toast.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="toast">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "GuluToast"
+};
+</script>
+<style lang="scss" scoped>
+$font-size: 14px;
+$toast-height: 40px;
+$toast-bg: rgba(0, 0, 0, 0.5);
+$toast-color:#fff;
+.toast {
+  position: fixed;
+  top: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  font-size: $font-size;
+  line-height: 1.8;
+  height: $toast-height;
+  display: flex;
+  align-items: center;
+  background: $toast-bg;
+  border-radius: 4px;
+  box-shadow: 0px 0px 3px 0px rgba(0, 0, 0, 0.5);
+  color: $toast-color;
+  padding: 0 16px;
+}
+</style>
